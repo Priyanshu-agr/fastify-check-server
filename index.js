5032d6fd58b75c932f2df95af1b402f0b20dea36
Priyanshu <agarwal3014@gmail.com>
2024-02-10 15:15:20 +0530

    added cors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Import the framework and instantiate it
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
 const fastify = Fastify({
     logger: true
 })
 
+await fastify.register(cors,{
+    origin:true  
+});
+
 // Declare a route
 fastify.get('/', async function handler(request, reply) {
     return {
-        
+        success: true,
         data: 'Server is up and running',
     }
 })
